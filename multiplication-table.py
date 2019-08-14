028da61201c02004e6d097f6a74caeda3c56c8a3
szymekj <54033714+szymekj@users.noreply.github.com>
2019-08-14 14:33:15 +0200

    Update multiplication-table.py
    fixed and nicer

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,5 +1,5 @@
-# Display multiplication table for numbers 1 to 10.
+# Display multiplication table for numbers 1 to 10. 
 print ("Multiplication Table")
 for l1 in range (1, 11):
     for a1 in range (1,11):
-        print ( str(a1) + " * " + (l1) + " = " + str(a1*l1))
+        print ( str(a1) + " * " + str(l1) + " = " + str(a1*l1))
